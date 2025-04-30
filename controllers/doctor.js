1805bf734b41ebdd850cdd4e1fa082185f1223e1
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2025-04-30 16:06:26 +0530

    get single, put, delete doctor

--- controllers/doctor.js
+++ controllers/doctor.js
@@ -1,3 +1,4 @@
+import { response } from "express";
 import { doctor } from "../models/doctor.js";
 
 export const addDoctor = async (req, res) => {
@@ -39,6 +40,48 @@ export const getAllDoctors = async (req, res) => {
     if (docs.length < 1)
       return res.status(404).json({ message: "no records found" });
     res.status(200).json(docs);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "internal server error", message: err.message });
+  }
+};
+
+export const getSingleDoctor = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const findDoc = await doctor.findById(id);
+    if (!findDoc) return res.status(404).json({ message: "doctor not found" });
+    res.status(200).json(findDoc);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "internal server error", message: err.message });
+  }
+};
+
+export const updateDoctor = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const findDoc = await doctor.findById(id);
+    if (!findDoc) return res.status(404).json({ message: "doctor not found" });
+    const doc = await doctor.findByIdAndUpdate(id, body, { new: true });
+    res.status(200).json(doc);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "internal server error", message: err.message });
+  }
+};
+
+export const deleteDoctor = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const findDoc = await doctor.findById(id);
+    if (!findDoc) return res.status(404).json({ message: "doctor not found" });
+    await doctor.findByIdAndDelete(id);
+    res.status(200).json({ message: "deleted successfully" });
   } catch (err) {
     res
       .status(500)
--- routes/doctor.js
+++ routes/doctor.js
@@ -1,9 +1,20 @@
 import express from "express";
-import { addDoctor, getAllDoctors } from "../controllers/doctor.js";
+import {
+  addDoctor,
+  deleteDoctor,
+  getAllDoctors,
+  getSingleDoctor,
+  updateDoctor,
+} from "../controllers/doctor.js";
 const router = express.Router();
 
 router.get("/getall", getAllDoctors);
+router.get("/getbyid/:id", getSingleDoctor);
 
 router.post("/add", addDoctor);
+
+router.put("/update/:id", updateDoctor);
+
+router.delete("/delete/:id", deleteDoctor);
 
 export default router;
